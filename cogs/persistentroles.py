a5f3739fdc62538fd3a9eab23d0439eca0405e08
siren15 <slowsc2@gmail.com>
2021-09-18 14:52:52 +0200

    fixed persistent roles not being saved to db and not being given to users

--- cogs/persistentroles.py
+++ cogs/persistentroles.py
@@ -5,50 +5,40 @@ from discord.ext import commands
 from customchecks import *
 from discord.utils import get
 
-class persistentroles(commands.Cog):
+class persistent_roles(commands.Cog):
     """Makes roles persistent when members leave, and will give them back when they rejoin."""
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        async for entry in member.guild.audit_logs(limit=1, action=discord.AuditLogAction.kick):
-            if entry.target.id == member.id:
-                return
-        try:
-            await member.guild.fetch_ban(member)
-        except discord.NotFound:
-            member_roles = [role for role in member.roles if role.name != '@everyone']
-            rolecount = len(member_roles)
-            if rolecount == 0:
-                return
-            else:
-                db = await odm.connect()
-                check = await db.find_one(persistentroles, {"guildid":member.guild.id, "userid":member.id})
-                if check == None:
-                    db.delete(check)
-
-                member_roles = ",".join([str(role.id) for role in member.roles if role.name != '@everyone'])
-                await db.save(persistentroles, {"guildid":member.guild.id, "userid":member.id, "roles":member_roles})
-        else:
+        member_roles = [role for role in member.roles if role.name != '@everyone']
+        rolecount = len(member_roles)
+        if rolecount == 0:
             return
+        else:
+            db = await odm.connect()
+            check = await db.find_one(persistentroles, {"guildid":member.guild.id, "userid":member.id})
+            if check == None:
+                member_roles = ",".join([str(role.id) for role in member.roles if role.name != '@everyone'])
+                await db.save(persistentroles(guildid=member.guild.id, userid=member.id, roles=member_roles))
+            else:
+                db.delete(check)
+                member_roles = ",".join([str(role.id) for role in member.roles if role.name != '@everyone'])
+                await db.save(persistentroles(guildid=member.guild.id, userid=member.id, roles=member_roles))
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        cluster = Mongo.connect()
-        db = cluster["giffany"]
-        table = db['persistentroles']
-        users = table.find({"guildid":member.guild.id, "userid":member.id})
-        for user in users:
-            if member.id == user['userid']:
-                roles = table.find({"guildid":member.guild.id, "userid":member.id})
-                roles = [r['roles'].split(',') for r in roles]
-                for roles in roles:
-                    roles = [member.guild.get_role(int(id_)) for id_ in roles if len(id_)]
-                    roles = roles + member.roles
-                    await member.edit(roles=roles)
-                    table.delete_one({"guildid":member.guild.id, "userid":member.id})
+        db = await odm.connect()
+        u = await db.find_one(persistentroles, {"guildid":member.guild.id, "userid":member.id})
+        if member.id == u.userid:
+            roles = [u.roles.split(',')]
+            for roles in roles:
+                roles = [member.guild.get_role(int(id_)) for id_ in roles if len(id_)]
+                roles = roles + member.roles
+                await member.edit(roles=roles)
+                db.delete(user)
 
 def setup(bot):
-    bot.add_cog(persistentroles(bot))
+    bot.add_cog(persistent_roles(bot))
     print('persistent roles module loaded')
