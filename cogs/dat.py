f7b303e885db4948a747afea11a4e7db535015cd
siren15 <slowsc2@gmail.com>
2021-11-02 19:45:51 +0100

    finished the submissions command

--- cogs/dat.py
+++ cogs/dat.py
@@ -147,91 +147,16 @@ class dat(commands.Cog):
     @commands.command()
     @commands.has_any_role('Mods')
     async def submissions(self, ctx):
-        def chunks(l, n):
-            n = max(1, n)
-            return (l[i:i+n] for i in range(0, len(l), n))
-
-        def create_list(lst, n, s, e):
-            lst = list(chunks(lst, n))
-            for i in lst[s:e]:
-                lst = i
-            fl = ''
-            for n in lst:
-                fl = fl + n
-            return fl
-
-        def newpage(names, counter, pages):
-            embed = Embed(description=f"__**Listing info about submissions for draw-a-thon 2021**__",
-            colour=0xF893B2)
-            embed.add_field(name="User names:", value=f"{names}")
-            embed.add_field(name="Submission counter:", value=f"{counter}")
-            embed.set_footer(text=pages)
-            return embed
-
-        def check(reaction, user):
-            return user == ctx.author and str(reaction.emoji) in ['⬅️', '➡️']
-
-
         cluster = Mongo.connect()
         db = cluster["giffany"]
-        tag_names = db['dat'].find({"guildid":ctx.guild.id})
-        names = [str(name['authorid'])+"\n" for name in tag_names]
-
-        counter = db['dat'].find({"guildid":ctx.guild.id})
-        counter =  [str(c['authorid'])+"\n" for c in counter]
-
-        if names == []:
-            embed = Embed(description=f"There are no submissions yet.",
-                          colour=0xF893B2)
-            await ctx.send(embed=embed)
-            return
-
-        s = 0
-        e = 1
-        counter = 1
-
-        nc = list(chunks(names, 20))
-
-        footer = f"Page:{counter}|{len(nc)}"
-        embedl = await ctx.send(embed=newpage(create_list(names, 20, s, e), ctx.guild, footer))
-        await embedl.add_reaction('⬅️')
-        await embedl.add_reaction('➡️')
-
-        while True:
-            try:
-                reaction, user = await self.bot.wait_for('reaction_add', timeout=60.0, check=check)
-
-            except asyncio.TimeoutError:
-                await embedl.clear_reactions()
-                break
-
-            if (reaction.emoji == '➡️') and (counter < len(nc)):
-                counter = counter + 1
-                s = s + 1
-                e = e + 1
-                footer = f"Page:{counter}|{len(nc)}"
-                await embedl.edit(embed=newpage(create_list(names, 20, s, e), ctx.guild, footer))
-                await embedl.remove_reaction('➡️', ctx.author)
-            else:
-                await embedl.remove_reaction('➡️', ctx.author)
-
-            if (reaction.emoji == '⬅️') and (counter > 1):
-                counter = counter - 1
-                s = s - 1
-                e = e - 1
-                footer = f"Page:{counter}|{len(nc)}"
-                await embedl.edit(embed=newpage(create_list(names, 20, s, e), ctx.guild, footer))
-                await embedl.remove_reaction('⬅️', ctx.author)
-            else:
-                await embedl.remove_reaction('⬅️', ctx.author)
-
-
-
-
-
-
-
-
+        table = db['dat']
+        names = table.find({"guildid":ctx.guild.id})
+        for n in names:
+            prompts = table.find({"guildid":ctx.guild.id, "authorid":n["authorid"]})
+            for p in prompts:
+                counter = table.count_documents({"guildid":ctx.guild.id, "authorid":n["authorid"]})
+                author = ctx.guild.get_member(p["authorid"])
+        await ctx.send(f"```\nAuthor: | Counter:\n{author}: {counter}\n```")
 
 
 def setup(bot):
